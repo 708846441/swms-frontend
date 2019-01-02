1f9ca9c310738ca0d82fb6b7bea402a452f09c74
derFischer <dingd2015@sjtu.edu.cn>
2019-01-02 22:21:00 +0800

    Update Global.js

--- src/Global.js
+++ src/Global.js
@@ -1,4 +1,4 @@
-export const dataApi = "http://www.test.com:7070/api"
+export const dataApi = "http://www.test.com/api"
 //export const dataApi = "http://localhost:7070/api"
 
 export const now = (new Date().toLocaleDateString()).replace(/\//g,'-')
